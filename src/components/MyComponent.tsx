b6621615d71d2cbfa6378a88a7a6f847e024c7f2
Jaypee Mayo <jaypeemayo@gmail.com>
2018-07-27 19:24:05 +1000

    test branch

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,20 +1,18 @@
 import * as React from "react";
 import "./../assets/scss/App.scss";
-import MyComponent from "./MyComponent";
 
 const reactLogo = require("./../assets/img/react_logo.svg");
 
-export interface AppProps {
+export interface IMyComponent {
 }
 
-export default class App extends React.Component<AppProps, undefined> {
+export default class MyComponent extends React.Component<IMyComponent, undefined> {
     render() {
         return (
             <div className="app">
                 <h1>Hello World!</h1>
                 <p>Foo to the barz test</p>
                 <img src={reactLogo} height="480"/>
-                <MyComponent/>
             </div>
         );
     }
