0a3c302d525633d318badac119ecdb9321521da4
Jaypee Mayo <jaypeemayo@gmail.com>
2018-07-31 21:05:54 +1000

    date time looks better

--- src/components/practioner/Appointment.tsx
+++ src/components/practioner/Appointment.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 import AppointmentDetails from "./AppointmentDetails";
 import { IAppointment } from "./IAppointment";
-
+import { FaChevronDown, FaChevronUp } from "../../../node_modules/react-icons/fa";
+let moment = require("moment");
+if ("default" in moment) {
+    moment = moment["default"];
+}
 export interface IAppointmentProps extends IAppointment {
 }
 
@@ -14,17 +18,27 @@ export default class Appointment extends React.Component<IAppointmentProps, IApp
     onClick = () => {
         this.setState({isAppointmentDetailsShown: !this.state.isAppointmentDetailsShown})
     }
+    renderChevron = () => {
+        if (this.state.isAppointmentDetailsShown) {
+            return <FaChevronUp />;
+        }
+        else {
+            return <FaChevronDown />;
+        }
+    }
     render() {
         return (
             <>
                 <tr onClick={this.onClick}>
-                    <th scope="row">{this.props.date}</th>
+                    <th scope="row">{moment(this.props.date).local().format("DD/MM/YYYY")}</th>
+                    <td>{moment(this.props.date).local().format("HH:mm A")}</td>
                     <td>{this.props.cost}</td>
                     <td>{this.props.revenue}</td>
+                    <td>{this.renderChevron()}</td>
 
                 </tr>
                 {this.state.isAppointmentDetailsShown && <tr>
-                    <td colSpan={3}>
+                    <td colSpan={5}>
                         <AppointmentDetails {...this.props}/>
                     </td>
                 </tr>}
--- src/components/practioner/Appointments.tsx
+++ src/components/practioner/Appointments.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import AppointmentDetails from "./AppointmentDetails";
 import { Table } from "../../common/Table";
 import Appointment from "./Appointment";
 import { IAppointment } from "./IAppointment";
+
 
 export interface IAppointmentsProps {
     appointments: IAppointment[];
@@ -12,11 +12,14 @@ export default class Appointments extends React.Component<IAppointmentsProps, un
     render() {
         return (
             <div className="border border-secondary">
+               
                 <h6 className="p-3 mb-2 bg-secondary text-white">Appointments</h6>
                 <Table head={<tr>
                     <th scope="col">Date</th>
+                    <th scope="col">Time</th>
                     <th scope="col">Cost</th>
                     <th scope="col">Revenue</th>
+                    <th scope="col"></th>
                 </tr>}>
                     <>
                         {this.props.appointments.map(appointment => <Appointment {...appointment} />)}
