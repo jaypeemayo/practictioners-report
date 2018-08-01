b82b9c5084c4edf69555d514f9669f51add856de
Jaypee Mayo <jaypeemayo@gmail.com>
2018-08-01 21:59:22 +1000

    updates

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,14 +8,13 @@ export interface AppProps {
 }
 
 
+//Read me:
+//go to directory where package.json is located then npm install
+//run <json-server --watch db.json> to start fake db
+//run <npm run start-dev> (link is shown in the command prompt eg. http://localhost:8084/)
+//go to the link
 
 export default class App extends React.Component<AppProps, undefined> {
-
-    componentDidMount(){
-     
-    }
-
-    
     render() {
         return <ReportPage/>;
     }
